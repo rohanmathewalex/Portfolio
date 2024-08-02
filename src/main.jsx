155553ef148d39a95d50fa0584a9bce74080492d
mrdiagodweb <mediagodweb@gmail.com>
2024-08-02 16:21:26 -0400

    posthog

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { PostHogProvider} from 'posthog-js/react' 
+
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_POSTHOG_API_KEY}
+      options={options}
+    ></PostHogProvider>
     <App />
   </React.StrictMode>,
 )
